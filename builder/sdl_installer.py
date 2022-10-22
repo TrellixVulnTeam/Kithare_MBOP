7a0d458b550c6c27b421bf08f040d3770ba05abe
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-22 11:08:52 +0000

    Adding tarfile member sanitization to extractall()

--- builder/sdl_installer.py
+++ builder/sdl_installer.py
@@ -113,7 +113,29 @@ class WindowsSDLInstaller(SDLInstaller):
         try:
             with io.BytesIO(downloaddata) as fileobj:
                 with tarfile.open(mode="r:gz", fileobj=fileobj) as tarred:
-                    tarred.extractall(self.sdl_dir)
+                    
+                    import os
+                    
+                    def is_within_directory(directory, target):
+                        
+                        abs_directory = os.path.abspath(directory)
+                        abs_target = os.path.abspath(target)
+                    
+                        prefix = os.path.commonprefix([abs_directory, abs_target])
+                        
+                        return prefix == abs_directory
+                    
+                    def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                    
+                        for member in tar.getmembers():
+                            member_path = os.path.join(path, member.name)
+                            if not is_within_directory(path, member_path):
+                                raise Exception("Attempted Path Traversal in Tar File")
+                    
+                        tar.extractall(path, members, numeric_owner=numeric_owner) 
+                        
+                    
+                    safe_extract(tarred, self.sdl_dir)
 
         except (tarfile.TarError, OSError):
             # some error while extracting
